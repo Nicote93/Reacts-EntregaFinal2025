466eb0767dd6f07ba8fcac0af2504c2d3d27a6d4
Nicote93 <nicolas.vite.1993@gmail.com>
2025-12-03 23:15:29 -0300

    Se agregan secciones de footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,10 +9,10 @@ const Footer = () => {
       {/* Seccion de enlaces */}
       <ul className={styles.footerNav}>
         <li>
-          <a href="#" className={styles.footerLink}>Acerca de Nosotros</a>
+          <a href="https://github.com/Nicote93/Reacts-EntregaFinal2025" className={styles.footerLink}>Acerca de Nosotros</a>
         </li>
         <li>
-          <a href="#" className={styles.footerLink}>Política de Privacidad</a>
+          <a href="https://ih1.redbubble.net/image.3165548305.9171/flat,750x,075,f-pad,750x1000,f8f8f8.jpg" className={styles.footerLink}>Política de Privacidad</a>
         </li>
       </ul>
       {/* Seccion de Copyright */}
